d4aeb24225089bc2f5b4bba82f02a67b31cc7d17
amit7309830152 <amit7309830152@gmail.com>
2021-07-22 16:43:56 +0530

    Update db.js

--- my_legacy/db/db.js
+++ my_legacy/db/db.js
@@ -1,8 +1,7 @@
 let mongoose = require('mongoose');
 
-// mongoose.connect('mongodb://localhost:27017/portfolio_query', { useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.connect(`mongodb+srv://amit_kumar:sapple@123@cluster0.zdwko.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
-
+mongoose.connect('mongodb://localhost:27017/portfolio_query', { useNewUrlParser: true, useUnifiedTopology: true })
+    // mongoose.connect(`mongodb+srv://amit_kumar:sapple@123@cluster0.zdwko.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 const db = mongoose.connection;
 db.on('error', (error) => {
     console.log('Database could not be connected ' + error)
